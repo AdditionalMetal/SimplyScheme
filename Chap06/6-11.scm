43de058b19897aa05e88c56af06666e4715f0c6c
Additional Metal <67547350+AdditionalMetal@users.noreply.github.com>
2022-08-17 08:07:04 -0400

    Chap 06 - third increment

--- Chap06/6-11.scm
+++ Chap06/6-11.scm
@@ -7,15 +7,11 @@
   )
 	
 
-;; 31 <- 1,  3, 5,    7, 8,    10,    12
-;; 30 <-     4,    6,       9,   , 11
-;; 29 <-   2 (leap)
-;; 28 <-   2 (non-leap)
 (define (valid-date? m d y)
-  (if (> d 0) 
+  (if (> d 0)
       (cond ((member? m '(1 3 5 7 8 10  12)) (<= d 31))
 	    ((member? m '(   4 6   9  11  )) (<= d 30))
-	    ;; only 2 (February) left...
+	    ;;             2                 
 	    (     (leap-year? y)             (<= d 29))
 	    ((not (leap-year? y))            (<= d 28))
 	    (else #f)
