7fd8fe40a2bc318ecfe5f915ca427f7a1e64ea4f
Additional Metal <67547350+AdditionalMetal@users.noreply.github.com>
2022-09-17 07:08:37 -0400

    Exercise 12-9

---
+++ Chap12/12-9.scm
@@ -0,0 +1,16 @@
+;; 12.9 Write a procedure real-words that takes a sentence as argument
+;;      and returns all the "real" words of the sentence, using the
+;;      same rule as the real-word? procedure from Chapter 1.
+
+;; From Chapter 1...
+(define (real-word? wd)
+  (not (member? wd '(a the an in of and for to with))))
+
+(define (real-words sent)
+  (if (empty? sent)
+      '()
+      (se (if (real-word? (first sent))
+	      (se (first sent) (real-words (bf sent)))
+	      (real-words (bf sent))))
+      )
+  )
